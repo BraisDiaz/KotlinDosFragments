249130011446dfe32457d8b4b89fac03a76d8644
BraisDiaz <braisdiaz@hotmail.com>
2018-02-13 18:07:05 +0100

    Proyecto terminado

--- app/src/main/java/org/danielcastelao/bdiaznunez/kotlindosfragments/MainFragment.kt
+++ app/src/main/java/org/danielcastelao/bdiaznunez/kotlindosfragments/MainFragment.kt
@@ -1,22 +1,86 @@
 package org.danielcastelao.bdiaznunez.kotlindosfragments
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
-import android.support.design.widget.Snackbar
-import android.support.v7.app.AppCompatActivity
-
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import kotlinx.android.synthetic.main.fragment_main.*
+//import org.jetbrains.anko.support.v4.toast
 
-class MainFragment : AppCompatActivity() {
+// para poder acceder a los elementos del layout
+// libreria Anko para facilitar (aún mas) código
+
+//import org.jetbrains.anko.support.v4.toast
+
+class MainFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.fragment_main)
-        setSupportActionBar(toolbar)
+    }
+        /**
+         * Cargamos el layout del fragment
+         */
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
+        override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?,
+                                  savedInstanceState: Bundle?): View? {
+            return inflater!!.inflate(R.layout.fragment_main, container, false)
+
         }
+
+        override fun onDetach() {
+
+            super.onDetach()
+
+        }
+
+        override fun onAttach(context: Context?) {
+
+            super.onAttach(context)
+
     }
 
+        /**
+         * Una vez cargado el layout podemos utilizar sus elementos
+         */
+
+        override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
+
+            super.onViewCreated(view, savedInstanceState)
+
+            // añadimos funcionalidad al boton
+
+            main_button.setOnClickListener {
+
+                // Mostramos una toast
+
+                // Para utilizar este método necesitamos la librería Anko que completa las funcionalidades de Kotlin
+
+                // https://github.com/Kotlin/anko
+
+                // es necesario
+
+                // toast("Mensaje: ${main_editText.text}")
+
+                // mostramos en el TextView lo que introducimos en el EditText
+
+                //toast("Mensaje: ${main_editText}.text")
+
+                main_textview.text = main_editText.text
+
+                // Utilizamos 'activity' para indicar el context,
+
+                // como es un fragment el context es la activity que lo contiene
+
+                val datos = Intent(activity,Main2Activity::class.java)
+
+                datos.putExtra("key1","valor1")
+
+                startActivity(datos)
+
+            }
+
+        }
 }
--- app/src/main/java/org/danielcastelao/bdiaznunez/kotlindosfragments/SecondFragment.kt
+++ app/src/main/java/org/danielcastelao/bdiaznunez/kotlindosfragments/SecondFragment.kt
@@ -1,4 +1,5 @@
 package org.danielcastelao.bdiaznunez.kotlindosfragments
+import android.content.Context
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
@@ -8,6 +9,14 @@ import android.view.ViewGroup
 class SecondFragment : Fragment() {
 
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+
+
+
+    }
+
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?,
 
                               savedInstanceState: Bundle?): View? {
@@ -15,6 +24,24 @@ class SecondFragment : Fragment() {
         // Inflate the layout for this fragment
 
         return inflater!!.inflate(R.layout.fragment_second, container, false)
+
+    }
+
+    override fun onAttach(context: Context?) {
+
+        super.onAttach(context)
+
+
+
+    }
+
+
+
+    override fun onDetach() {
+
+        super.onDetach()
+
+
 
     }
 
